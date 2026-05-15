ebc279185172b34693e8972565c07159fbed547a
posicionaando-maker <posiciona.ando@gmail.com>
2026-05-15 15:07:57 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'mercaditov4-v2';
+const urlsToCache = [
+  '/MercaditoV4/',
+  '/MercaditoV4/index.html',
+  'https://cdn.jsdelivr.net/npm/idb@8.0.0/build/umd.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
